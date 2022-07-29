4ccaf74b65fb5353f1f95d1a8eca5304276b41b7
unknown <thdud8866@naver.com>
2022-07-29 15:39:11 +0900

    시간 변환 프로그램

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [minutes, setMinutes] = useState(0);
+  const [active, setActive] = useState(false);
+  const reset = () => {
+    setMinutes = 0;
+  };
+  const onChange = (event) => {
+    setMinutes(event.taget.value);
+  };
+  const changeActive = () => {
+    setActive(!active);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Time Converter</div>
+      <div>
+        Minutes :{" "}
+        <input
+          placeholder="Minutes"
+          value={minutes}
+          type="number"
+          onChange={onChange}
+          disabled={active}
+        />
+        <div>
+          Hours :
+          <input
+            type="number"
+            placeholder="Hours"
+            value={Math.floor(minutes / 60)}
+            disabled={!active}
+          />
+        </div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={changeActive}>active toggle</button>
+      </div>
     </div>
   );
 }
